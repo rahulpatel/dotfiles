b4cb9c8b29ba8ac8f80c4edae6af959c8a6c8835
rahulpatel <rahulpatel@users.noreply.github.com>
2021-11-23 16:35:37 +0000

    Fix typo

--- homebrew/config.zsh
+++ homebrew/config.zsh
@@ -1,4 +1,4 @@
-eval "$(/opt/homebrew/bin/brew sheellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Add homebrew autocomplete to FPATH, compinit called in zsh/zshrc.symlink
 FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
