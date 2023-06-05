bee7573e1797d335b1fa7f986666d4e5aa30b42a
Rahul Patel <rahul.patel2@virginmediao2.co.uk>
2023-06-05 18:21:35 +0100

    feat: store code directly in ~/Developer

--- macos/init.sh
+++ macos/init.sh
@@ -12,32 +12,43 @@ cd "$(dirname "$0")"
 main() {
     echo "ℹ️  macos"
 
-    local repo_volume_name="Repositories"
-    local repo_symlink_name="Developer"
+    local code_folder_path="$HOME/Developer"
 
-    local repo_volume_path="/Volumes/$repo_volume_name"
-    local repo_symlink_path="$HOME/$repo_symlink_name"
-
-    if [[ -d "$repo_volume_path" ]]; then
+    if [[ -d "$code_folder_path" ]]; then
         echo "> repositories volume already exists"
         exit
     fi
 
-    diskutil list
-    echo '> which disk should the volume be added to?'
-    read -r -e disk_name
-
-    if [[ $disk_name == "" ]]; then
-        echo '> disk name not provided'
-        exit 1
+    if [[ ! -d "$code_folder_path" ]]; then
+        mkdir "$code_folder_path"
     fi
 
-    diskutil apfs addVolume "$disk_name" "Case-sensitive APFS" "$repo_volume_name" -passprompt
+    # local repo_volume_name="Repositories"
+    # local repo_symlink_name="Developer"
 
-    if [[ ! -L "$repo_symlink_path" ]]; then
-        mkdir "$repo_symlink_path"
-        ln -s "$repo_volume_path" "$repo_symlink_path"
-    fi
+    # local repo_volume_path="/Volumes/$repo_volume_name"
+    # local repo_symlink_path="$HOME/$repo_symlink_name"
+
+    # if [[ -d "$repo_volume_path" ]]; then
+    #     echo "> repositories volume already exists"
+    #     exit
+    # fi
+
+    # diskutil list
+    # echo '> which disk should the volume be added to?'
+    # read -r -e disk_name
+
+    # if [[ $disk_name == "" ]]; then
+    #     echo '> disk name not provided'
+    #     exit 1
+    # fi
+
+    # diskutil apfs addVolume "$disk_name" "Case-sensitive APFS" "$repo_volume_name" -passprompt
+
+    # if [[ ! -L "$repo_symlink_path" ]]; then
+    #     mkdir "$repo_symlink_path"
+    #     ln -s "$repo_volume_path" "$repo_symlink_path"
+    # fi
 
     echo "✅ macos"
 }
