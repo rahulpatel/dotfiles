71bee7b04a83ef30b6cc56dbb6fc614e2429f813
Rahul Patel <rahulpatel@users.noreply.github.com>
2023-10-06 15:59:57 +0100

    chore: fix merge issues

--- macos/init.sh
+++ macos/init.sh
@@ -12,45 +12,6 @@ cd "$(dirname "$0")"
 main() {
     echo "ℹ️  macos"
 
-<<<<<<< Updated upstream
-    local code_folder_path="$HOME/Developer"
-
-    if [[ -d "$code_folder_path" ]]; then
-        echo "> repositories volume already exists"
-        exit
-    fi
-
-    if [[ ! -d "$code_folder_path" ]]; then
-        mkdir "$code_folder_path"
-    fi
-
-    # local repo_volume_name="Repositories"
-    # local repo_symlink_name="Developer"
-
-    # local repo_volume_path="/Volumes/$repo_volume_name"
-    # local repo_symlink_path="$HOME/$repo_symlink_name"
-
-    # if [[ -d "$repo_volume_path" ]]; then
-    #     echo "> repositories volume already exists"
-    #     exit
-    # fi
-
-    # diskutil list
-    # echo '> which disk should the volume be added to?'
-    # read -r -e disk_name
-
-    # if [[ $disk_name == "" ]]; then
-    #     echo '> disk name not provided'
-    #     exit 1
-    # fi
-
-    # diskutil apfs addVolume "$disk_name" "Case-sensitive APFS" "$repo_volume_name" -passprompt
-
-    # if [[ ! -L "$repo_symlink_path" ]]; then
-    #     mkdir "$repo_symlink_path"
-    #     ln -s "$repo_volume_path" "$repo_symlink_path"
-    # fi
-=======
     local repos_folder_path="$HOME/Developer"
 
     if [[ -d "$repos_folder_path" ]]; then
@@ -58,7 +19,6 @@ main() {
     fi
 
     mkdir "$repos_folder_path"
->>>>>>> Stashed changes
 
     echo "✅ macos"
 }
