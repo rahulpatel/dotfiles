318d3fa34f7154cf7b37267545fd6fdc4db64bbf
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-03-21 21:44:26 +0000

    feat: switch to using rose-pine-moon theme

--- config/nvim.symlink/init.lua
+++ config/nvim.symlink/init.lua
@@ -734,11 +734,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    'rose-pine/neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'rose-pine-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
