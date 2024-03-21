e3dcfd0dea38b800f4e077c946f63d76194ae9b9
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-03-21 22:48:12 +0000

    feat: nvim config improvements

--- config/nvim.symlink/init.lua
+++ config/nvim.symlink/init.lua
@@ -619,7 +619,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
@@ -691,7 +691,8 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<TAB>'] = cmp.mapping.confirm { select = true },
+          ['<ENTER>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
