a493754c4323f1700af8b266dd917621831481b3
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-03-27 21:50:44 +0000

    feat: improve nvim copilot and add typescript tools

--- config/nvim.symlink/lua/custom/plugins/copilot.lua
+++ config/nvim.symlink/lua/custom/plugins/copilot.lua
@@ -4,7 +4,21 @@ return {
     cmd = 'Copilot',
     event = 'InsertEnter',
     config = function()
-      require('copilot').setup {}
+      require('copilot').setup {
+        suggestion = {
+          auto_trigger = true,
+        },
+      }
+
+      local cmp = require 'cmp'
+
+      cmp.event:on('menu_opened', function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+
+      cmp.event:on('menu_closed', function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
     end,
   },
 }
