3e34266a132daaa5da650e9b81f908fc48b680dc
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-08-22 16:19:29 +0100

    fix: code function

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -1,23 +1,23 @@
 function code
-    set selected (find $HOME $CODE_DIR -mindepth 1 -maxdepth 1 -type d | fzf --query $argv --select-1)
+    if test -z $argv
+        set argv ""
+    end
+
+    set selected (find $HOME $CODE_DIR -mindepth 1 -maxdepth 1 -type d | fzf --select-1 --query $argv)
 
     if test -z $selected
-        exit
+        return 0
     end
 
     set name (basename $selected | tr . _)
 
+    if not test -z (pgrep tmux) || not tmux has-session -t $name 2>/dev/null
+        tmux new-session -ds $name -c $selected
+    end
+
     if test -z $TMUX
-        if tmux has-session -t=$name 2>/dev/null
-            tmux attach-session -t $name
-        else if test (count (pgrep tmux)) -ne 0
-            tmux new-session -ds $name -c $selected
-        end
+        tmux attach-session -t $name
     else
-        if tmux has-session -t=$name 2>/dev/null
-            tmux switch-client -t $name
-        else
-            tmux new-session -ds $name -c $selected
-        end
+        tmux switch-client -t $name
     end
 end
