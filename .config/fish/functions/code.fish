d967c83b863cf7f824ffd084125034c0a0205a4e
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-09-10 17:49:48 +0100

    fix: stop logging errors with code command

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -3,7 +3,7 @@ function code
         set argv ""
     end
 
-    set selected (find $HOME $CODE_DIR -mindepth 1 -maxdepth 1 -type d | fzf --select-1 --query $argv)
+    set selected (find $HOME $CODE_DIR $CODE_DIR/work -mindepth 1 -maxdepth 1 -type d | fzf --select-1 --query $argv)
 
     if test -z $selected
         return 0
@@ -11,13 +11,9 @@ function code
 
     set name (basename $selected | tr . _)
 
-    if not test -z (pgrep tmux) || not tmux has-session -t $name 2>/dev/null
-        tmux new-session -ds $name -c $selected
-    end
-
     if test -z $TMUX
-        tmux attach-session -t $name
+        tmux attach-session -t $name 2>/dev/null || tmux new-session -s $name -c $selected
     else
-        tmux switch-client -t $name
+        tmux switch-client -t $name 2>/dev/null || tmux new-session -s $name -c $selected
     end
 end
