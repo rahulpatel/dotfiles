7edb11c89e8ab798e8bb1c8a6531d6f524231fad
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-09-24 11:40:04 +0100

    refactor(nvim): move colorscheme to variable

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -14,6 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local colorscheme = "catppuccin-frappe"
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
@@ -21,7 +23,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "catppuccin-frappe",
+        colorscheme = colorscheme,
       },
     },
     -- import/override with your plugins
@@ -36,7 +38,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin-frappe" } },
+  install = { colorscheme = { colorscheme } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
