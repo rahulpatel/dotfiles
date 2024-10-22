044c72a04a5af5b19e396a18b849731e62568e9f
Rahul Patel <rahulpatel@users.noreply.github.com>
2024-10-22 21:47:55 +0100

    fix(fish): only add path to /etc/shells if required

--- .config/fish/install.sh
+++ .config/fish/install.sh
@@ -4,15 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+  set -o xtrace
 fi
 
 cd "$(dirname "$0")"
 
 main() {
-    local fish_path=/opt/homebrew/bin/fish
+  local fish_path=/opt/homebrew/bin/fish
+  if [[ -z $(cat /etc/shells | grep "$fish_path") ]]; then
     echo "$fish_path" | sudo tee -a /etc/shells
     chsh -s "$fish_path"
+  fi
 }
 
 main "$@"
