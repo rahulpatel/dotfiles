cfdd52951aff9c86d850e97bca11bb62c74f4a57
Rahul Patel <rahulpatel@users.noreply.github.com>
2025-01-29 10:10:26 +0000

    fix(asdf): install nodejs plugin

--- .config/asdf/install.sh
+++ .config/asdf/install.sh
@@ -4,13 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+  set -o xtrace
 fi
 
 cd "$(dirname "$0")"
 
 main() {
-    asdf install
+  asdf plugin add nodejs
+  asdf install
 }
 
 main "$@"
