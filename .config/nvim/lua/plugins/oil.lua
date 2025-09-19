d8daa69fbc1e7d4e22ac7a0b98c679f0784d06fd
Rahul Patel <rahulpatel@users.noreply.github.com>
2025-09-19 11:27:11 +0100

    fix(nvim): update to lazyvim v15

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
   "stevearc/oil.nvim",
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false,
   ---@module 'oil'
   ---@type oil.SetupOpts
