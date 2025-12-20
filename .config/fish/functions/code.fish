c75cefe97f3a9b95365106994663bd6903614cc6
Rahul Patel <rahulpatel@users.noreply.github.com>
2025-12-20 11:32:12 +0000

    feat(fish): update code switcher for new dev workflow

--- .config/fish/functions/code.fish
+++ .config/fish/functions/code.fish
@@ -5,7 +5,12 @@ function code
 
     set -l query (string replace '/' '' $argv)
 
-    set selected (find $HOME $CODE_DIR -mindepth 1 -maxdepth 2 -type d | fzf --select-1 --query $query)
+    set -l selected (
+        begin
+            find "$HOME" -mindepth 1 -maxdepth 1 -type d
+            find "$CODE_DIR" -mindepth 1 -maxdepth 2 -type d
+        end | fzf --select-1 --query "$query"
+    )
 
     if test -z $selected
         return 0
@@ -18,8 +23,6 @@ function code
 
         tmux send-keys -t $name:1 'nvim .' C-m
         tmux new-window -t $name:2 -c $selected
-        tmux new-window -t $name:3 -c $selected
-        tmux new-window -t $name:4 -c $selected
 
         tmux select-window -t $name:1
     end
