a0eaf27b0db3813fab6ca42792cc20d9d8d91c98
Rahul Patel <rahulpatel@users.noreply.github.com>
2026-02-07 09:29:57 +0000

    chore: format and update ghostty cask name

--- install.sh
+++ install.sh
@@ -4,35 +4,34 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+  set -o xtrace
 fi
 
 cd "$(dirname "$0")"
 
 main() {
-    local CONFIGDIR="$PWD/.config"
+  local CONFIGDIR="$PWD/.config"
 
-    echo "setup macos"
-    /bin/bash "$CONFIGDIR/macos/install.sh"
+  echo "setup macos"
+  /bin/bash "$CONFIGDIR/macos/install.sh"
 
+  echo "setup homebrew"
+  /bin/bash "$CONFIGDIR/homebrew/install.sh"
 
-    echo "setup homebrew"
-    /bin/bash "$CONFIGDIR/homebrew/install.sh"
+  # activate homebrew in this session
+  source "$PWD/.config/homebrew/config.sh"
 
-    # activate homebrew in this session
-    source "$PWD/.config/homebrew/config.sh"
+  mkdir -p "$HOME/.config"
+  stow -t "$HOME" -v .
 
-    mkdir -p "$HOME/.config"
-    stow -t "$HOME" -v .
+  local install_files=$(find "$CONFIGDIR" -name "install.sh" ! -path "*macos*" ! -path "*homebrew*" ! -path "*zsh*")
+  for file in $install_files; do
+    echo "setup $file"
+    /bin/bash "$file"
+  done
 
-    local install_files=$(find "$CONFIGDIR" -name "install.sh" ! -path "*macos*" ! -path "*homebrew*" ! -path "*zsh*")
-    for file in $install_files; do
-        echo "setup $file"
-        /bin/bash "$file"
-    done
-
-    echo ""
-    echo "Restart the machine"
+  echo ""
+  echo "Restart the machine"
 }
 
 main "$@"
