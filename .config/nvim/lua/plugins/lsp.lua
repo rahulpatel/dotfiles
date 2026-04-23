a133e15119c3e1cbccaad787e800fe7da8f5a64c
Rahul Patel <rahulpatel@users.noreply.github.com>
2026-04-23 08:47:29 +0100

    feat(nvim): set vtsls config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,4 +3,19 @@ return {
   opts = {
     inlay_hints = { enabled = false },
   },
+  init = function()
+    vim.lsp.config("vtsls", {
+      settings = {
+        typescript = { tsserver = { maxTsServerMemory = 8192 } },
+        vtsls = {
+          autoUseWorkspaceTsdk = true,
+          experimental = {
+            completion = {
+              enableServerSideFuzzyMatch = true,
+            },
+          },
+        },
+      },
+    })
+  end,
 }
